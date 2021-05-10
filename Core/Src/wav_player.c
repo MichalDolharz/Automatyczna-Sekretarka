4c37bfd947304af20056fb25be4515e05c90d6ed
Dawid Jarzabek <248991@student.pwr.edu.pl>
2021-05-10 02:06:06 +0200

    Synchronizacja v2

--- Core/Src/wav_player.c
+++ Core/Src/wav_player.c
@@ -12,6 +12,7 @@ static __IO uint32_t audioRemainSize = 0;
 
 static UINT playerReadBytes = 0;
 static bool isFinished = 0;
+static uint32_t samplingFreq;
 
 //stany transmisji
 typedef enum {
@@ -23,6 +24,76 @@ typedef enum {
 static volatile PLAYER_CONTROL_e playerControlSM = PLAYER_CONTROL_Idle;
 
 static I2S_HandleTypeDef *hAudioI2S;
+
+extern const uint32_t I2SFreq[8];
+extern const uint32_t I2SPLLN[8];
+extern const uint32_t I2SPLLR[8];
+
+//Aktualizacja konfiguracji zegarów w zależności od częstotliwości pliku dźwiękowego
+static void audioI2S_pllClockConfig(uint32_t audioFreq)
+{
+  RCC_PeriphCLKInitTypeDef rccclkinit;
+  uint8_t index = 0, freqindex = 0xFF;
+
+  for(index = 0; index < 8; index++)
+  {
+    if(I2SFreq[index] == audioFreq)
+    {
+      freqindex = index;
+    }
+  }
+
+  HAL_RCCEx_GetPeriphCLKConfig(&rccclkinit);
+
+  if ((freqindex & 0x7) == 0)
+  {
+    rccclkinit.PeriphClockSelection = RCC_PERIPHCLK_I2S;
+    rccclkinit.PLLI2S.PLLI2SN = I2SPLLN[freqindex];
+    rccclkinit.PLLI2S.PLLI2SR = I2SPLLR[freqindex];
+    HAL_RCCEx_PeriphCLKConfig(&rccclkinit);
+  }
+  else
+  {
+    rccclkinit.PeriphClockSelection = RCC_PERIPHCLK_I2S;
+    rccclkinit.PLLI2S.PLLI2SN = 258;
+    rccclkinit.PLLI2S.PLLI2SR = 3;
+    HAL_RCCEx_PeriphCLKConfig(&rccclkinit);
+  }
+}
+
+//aktualizacja parametru częstotliwości w I2S3
+static bool I2S3_freqUpdate(uint32_t AudioFreq)
+{
+  hAudioI2S->Instance         = SPI3;
+
+  __HAL_I2S_DISABLE(hAudioI2S);
+
+  hAudioI2S->Init.AudioFreq   = AudioFreq;
+  hAudioI2S->Init.ClockSource = I2S_CLOCK_PLL;
+  hAudioI2S->Init.CPOL        = I2S_CPOL_LOW;
+  hAudioI2S->Init.DataFormat  = I2S_DATAFORMAT_16B;
+  hAudioI2S->Init.MCLKOutput  = I2S_MCLKOUTPUT_ENABLE;
+  hAudioI2S->Init.Mode        = I2S_MODE_MASTER_TX;
+  hAudioI2S->Init.Standard    = I2S_STANDARD_PHILIPS;
+
+  if(HAL_I2S_Init(hAudioI2S) != HAL_OK)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+//inicjalizacja I2S w zależności od częstotliwośći pliku dźwiękowego
+bool audioI2S_init(uint32_t audioFreq)
+{
+  audioI2S_pllClockConfig(audioFreq);
+  I2S3_freqUpdate(audioFreq);
+  return true;
+}
+
 
 //ustawienie uchwytu i2s
 void audioI2S_setHandle(I2S_HandleTypeDef *pI2Shandle) {
@@ -58,12 +129,14 @@ bool wavPlayer_fileSelect(const char *filePath) {
 	}
 	f_read(&wavFile, &wavHeader, sizeof(wavHeader), &readBytes);
 	fileLength = wavHeader.FileSize;
+	samplingFreq = wavHeader.SampleRate;
 	return true;
 }
 
 //start odtwarzania
 void wavPlayer_play(void) {
 	isFinished = false;
+	audioI2S_init(samplingFreq);
 	f_lseek(&wavFile, 0);
 	f_read(&wavFile, &audioBuffer[0], AUDIO_BUFFER_SIZE, &playerReadBytes);
 	audioRemainSize = fileLength - playerReadBytes;
