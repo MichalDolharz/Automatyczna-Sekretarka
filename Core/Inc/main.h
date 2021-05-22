d86d7a89a7281b4a2da88cb19143338758cbf9e6
Michał Dołharz <michal@atu-reklama.pl>
2021-05-22 13:54:01 +0200

    Zaktualizowano konfigurację LCD

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LCD_E_Pin GPIO_PIN_13
+#define LCD_E_GPIO_Port GPIOC
+#define LCD_RS_Pin GPIO_PIN_15
+#define LCD_RS_GPIO_Port GPIOC
 #define Blue_Button_Pin GPIO_PIN_0
 #define Blue_Button_GPIO_Port GPIOA
 #define Blue_Button_EXTI_IRQn EXTI0_IRQn
@@ -69,18 +73,14 @@ void Error_Handler(void);
 #define LED_Red_GPIO_Port GPIOD
 #define LED_Blue_Pin GPIO_PIN_15
 #define LED_Blue_GPIO_Port GPIOD
-#define LCD_D7_Pin GPIO_PIN_0
+#define LCD_D7_Pin GPIO_PIN_6
 #define LCD_D7_GPIO_Port GPIOD
-#define LCD_D6_Pin GPIO_PIN_2
-#define LCD_D6_GPIO_Port GPIOD
-#define LCD_D5_Pin GPIO_PIN_3
-#define LCD_D5_GPIO_Port GPIOD
-#define LCD_D4_Pin GPIO_PIN_6
-#define LCD_D4_GPIO_Port GPIOD
-#define LCD_E_Pin GPIO_PIN_3
-#define LCD_E_GPIO_Port GPIOB
-#define LCD_RW_Pin GPIO_PIN_5
-#define LCD_RW_GPIO_Port GPIOB
+#define LCD_D6_Pin GPIO_PIN_3
+#define LCD_D6_GPIO_Port GPIOB
+#define LCD_D5_Pin GPIO_PIN_5
+#define LCD_D5_GPIO_Port GPIOB
+#define LCD_D4_Pin GPIO_PIN_7
+#define LCD_D4_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
